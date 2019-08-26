054f5e0eca465471a4b01c3806cf69fd372e37c2
bhaswanth97 <bhaswanth.nexevo@gmail.com>
2019-08-26 11:33:03 +0530

    Created structure of config file

---
+++ app/Config/config.php
@@ -0,0 +1,14 @@
+<?php
+    return array(
+        /* Unique key for Application */
+        "APP_KEY" => "",
+
+        /* Location of Application */
+        "APP_PATH" => "",
+
+        /* Name of the Application */
+        "APP_NAME" => "",
+
+        /* Current mode of Application. Ex:- production, development, testing */
+        "APP_MODE" => "",
+    );
