80bfcbd797ba5c11d87a15215522092752289cfc
bhaswanth97 <bhaswanth.nexevo@gmail.com>
2019-09-19 18:21:11 +0530

    Modified controller

--- app/Core/Controller.php
+++ app/Core/Controller.php
@@ -13,7 +13,7 @@
         {
             extract($this->vars);
             ob_start();
-            require(ROOT . "Views/" . ucfirst(str_replace('Controller', '', get_class($this))) . '/' . $filename . '.php');
+            require(ROOT . "views/" . ucfirst(str_replace('Controller', '', get_class($this))) . '/' . $filename . '.php');
             $content_for_layout = ob_get_clean();
 
             if ($this->layout == false)
